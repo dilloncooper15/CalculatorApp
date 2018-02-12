7e4df385ec5d4db4625ff50ce12563bdf4b84e9b
Dillon Cooper <dillon.cooper@twomen.com>
2018-02-12 08:42:28 -0500

    Added textSize to Neg button and removed toast message

--- app/src/main/java/com/example/dillonc/calculator/MainActivity.kt
+++ app/src/main/java/com/example/dillonc/calculator/MainActivity.kt
@@ -94,7 +94,7 @@ class MainActivity : AppCompatActivity() {
             } else {
                 newEntryNumber.setText("")
                 operand2 = 0.0
-                toast("bll")
+//                toast("bll")
             }
         })
 
